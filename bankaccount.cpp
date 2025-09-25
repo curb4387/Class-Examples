03f97d645fe5b9a5dd9219c48855981159cfc6f8
curb4387 <teenawtran@gmail.com>
2025-09-24 20:04:26 -0400

    Outline needed parts of BankAccount class

---
+++ bankaccount.cpp
@@ -0,0 +1,6 @@
+/*
+    This class simulates a banking application to view an account's balance,
+    and deposit and withdraw funds.
+*/
+// data members: account number, balance
+// member functions: get account number, get balance, deposit, withdraw
